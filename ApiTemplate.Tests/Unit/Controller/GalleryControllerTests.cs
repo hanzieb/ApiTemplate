180ed12ca57b6ccebb5e83b0c43504baf06ced1b
hanzie <hanzi@hanzie-box>
2025-07-15 16:23:58 -0700

    #added unit test library #added tests for the pet gallery service and gallery controller

---
+++ ApiTemplate.Tests/Unit/Controller/GalleryControllerTests.cs
@@ -0,0 +1,55 @@
+﻿using ApiTemplate.Business.AppServices;
+using ApiTemplate.Business.ViewModels;
+using ApiTemplate.Model.EF.Entities;
+using ApiTemplate.Model.Models;
+using ApiTemplate.Web.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ApiTemplate.Tests.Unit.Controller
+{
+    [TestFixture]
+    public class GalleryControllerTests
+    {
+        private GalleryController _testableController;
+        private IPetGalleryService _mockPetGalleryService;
+
+        private AnimalViewModel _testAnimalViewModel;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var _testPhoto = new Photo("test.jpg", 0, 768, 1024);
+            var _testAnimal = new Animal("Test",
+                "The Test Animal", "Test", AnimalTypes.Dog, new List<Photo>() { _testPhoto });
+            _testAnimalViewModel = new AnimalViewModel(_testAnimal);
+
+            _mockPetGalleryService = Substitute.For<IPetGalleryService>();
+            _mockPetGalleryService.GetAll(CancellationToken.None)
+                .Returns(new AnimalViewModel[] { _testAnimalViewModel });
+
+            _testableController = new GalleryController(_mockPetGalleryService);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _testableController.Dispose();
+        }
+
+        [Test]
+        public async Task DoesReturnViewModel()
+        {
+            var view = await _testableController.Index(CancellationToken.None);
+            object? result = ((ViewResult)view).ViewData.Model;
+            //data accuracy testing is done at the service level...
+            //and doesn't need to be repeated at the controller...
+            //unless additional transformations happen during the call warranting testing on.
+            AnimalViewModel[] viewModel = result as AnimalViewModel[];
+            Assert.That(result, Is.TypeOf<AnimalViewModel[]>());
+            Assert.That(viewModel, Is.Not.Null);
+            Assert.That(viewModel.Count(), Is.EqualTo(1));
+        }
+    }
+}
---
+++ ApiTemplate.Tests/Unit/Services/PetGalleryServiceTests.cs
@@ -0,0 +1,61 @@
+﻿using ApiTemplate.Business.AppServices;
+using ApiTemplate.Business.Repositories;
+using ApiTemplate.Model.EF.Entities;
+using ApiTemplate.Model.Models;
+using NSubstitute;
+using NUnit.Framework;
+using System.Collections;
+
+namespace ApiTemplate.Tests.Unit.Services
+{
+    [TestFixture]
+    public class PetGalleryServiceTests
+    {
+        private PetGalleryService _testService;
+
+        private IAnimalRepository _mockAnimalRepository;
+        private IPhotoRepository _mockPhotoRepository;
+
+        private Photo _testPhoto;
+        private Animal _testAnimal;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _testPhoto = new Photo("test.jpg", 0, 768, 1024);
+            _testAnimal = new Animal("Test",
+                "The Test Animal", "Test", AnimalTypes.Dog, new List<Photo>() { _testPhoto });
+
+            _mockAnimalRepository = Substitute.For<IAnimalRepository>();
+            _mockAnimalRepository.Query()
+                .Returns(new List<Animal>() { _testAnimal }.AsQueryable());
+
+            _mockPhotoRepository = Substitute.For<IPhotoRepository>();
+
+            _testService = new PetGalleryService(_mockAnimalRepository, _mockPhotoRepository);
+        }
+
+        [Test]
+        public async Task ShouldReturnViewModelCollection()
+        {
+            var result = await _testService.GetAll(CancellationToken.None);
+            var record = result.FirstOrDefault();
+
+            Assert.That(result.Count(), Is.EqualTo(1));
+            Assert.That(record, Is.Not.Null);
+
+            Assert.That(record.Name, Is.EqualTo(_testAnimal.Name));
+            Assert.That(record.Description, Is.EqualTo(_testAnimal.Description));
+            Assert.That(record.Breed, Is.EqualTo(_testAnimal.Breed));
+            Assert.That(record.AnimalType, Is.EqualTo(_testAnimal.AnimalType));
+
+            var photo = record.Photos.FirstOrDefault();
+            Assert.That(record.Photos.Count(), Is.EqualTo(1));
+            Assert.That(photo, Is.Not.Null);
+            Assert.That(photo.FilePath, Is.EqualTo(_testPhoto.FilePath));
+            Assert.That(photo.Index, Is.EqualTo(_testPhoto.Index));
+            Assert.That(photo.Width, Is.EqualTo(_testPhoto.Width));
+            Assert.That(photo.Height, Is.EqualTo(_testPhoto.Height));
+        }
+    }
+}
